425af1d64cb74483ff25bc943625c68f980e0a27
SuzanneRashka <suzanne.rashka@gmail.com>
2018-08-02 17:42:15 -0500

    working on displaying questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
-    console.log("ready!");
-    // onclicks go here
-    $("#start").on("click", function () {
-        startGame();
-        nextQuestion();
-    })
+  console.log("ready!");
+  // onclicks go here
+  $("#start").on("click", function () {
+    startGame();
+    $(".wrapper").show();
+    console.log("within onclick - start");
+    $(this).hide();
+  });
 });
 
 var timeMax = 3;
@@ -13,88 +15,113 @@ var timerOutput;
 var timerMessage;
 var questionOutput;
 var optionOutput;
-
-var trivia = {
-    questions: {
-        q1: 'Who?',
-        q2: 'What?'
-    },
-    options: {
-        q1: ['one', 'two', 'three', 'four'],
-        q2: ['one', 'two', 'three', 'four']
-    },
-    answers: {
-        q1: 'one',
-        q2: 'two'
-        // q2: options.q2[1]
-    }
-};
-
-// countdown timer for each question
-function countdown() {
-    timer = setInterval(countdown, 1000)
-    //catch end of count
-    if (timeMax <= 0) {
-        clearTimeout();
-        timerMessage = setTimeout(function () {
-            $("#timer").html('<h1>Time\'s Up</h1>');
-        }, 1000);
-    } else {
-        timerOutput = $("#timer").html('<h1>' + timeMax + ' seconds remaining</h1>');
-        timeMax--;
-        console.log(timeMax);
-        // console.log(trivia.answers.q2);
-    }
-}
+var userAnswer;
+var message = "Game Over!";
+var trivia = [{
+    question: "What is 2*5?",
+    choices: [2, 5, 10, 15, 20],
+    correctAnswer: 2
+  },
+  {
+    question: "What is 3*6?",
+    choices: [3, 6, 9, 12, 18],
+    correctAnswer: 4
+  },
+  {
+    question: "What is 8*9?",
+    choices: [72, 99, 108, 134, 156],
+    correctAnswer: 0
+  },
+  {
+    question: "What is 1*7?",
+    choices: [4, 5, 6, 7, 8],
+    correctAnswer: 3
+  },
+  {
+    question: "What is 8*8?",
+    choices: [20, 30, 40, 50, 64],
+    correctAnswer: 4
+  }
+];
 
 function startGame() {
-    countdown();
-    clearTimeout();
-    console.log("startGame invoked");
+  console.log("startGame invoked");
+  countdown();
+  clearTimeout();
 
-    // $("#start").append('<button>Start</button>');
-    // start button invokes...
-    // nextQuestion()
+  // $("#start").append('<button>Start</button>');
+  // start button invokes...
+  // nextQuestion()
+}
+// initiates countdown
+function run() {
+  timer = setInterval(countdown, 1000);
+}
+// countdown timer for each question
+function countdown() {
+  $("#start").hide();
+  run();
+  //catch end of count
+  if (timeMax <= 0) {
+    clearTimeout();
+    timerMessage = setTimeout(function () {
+      $("#timer").html("<h1>Time's Up</h1>");
+    }, 1000);
+  } else {
+    timerOutput = $("#timer").html(
+      "<h1>" + timeMax + " seconds remaining</h1>"
+    );
+    timeMax--;
+    console.log(timeMax);
+  }
 }
 
+function display(data) {
+  var questionData = "<form id='qOne'>" + data.question + "<br>";
+  var choices = data.choices;
+
+  for (var i = 0; i < choices.length; i++) {
+    var choice = choices[i];
+    questionData += '<input type="radio" ' + choice;
+  }
+  return questionData + "</form>";
+}
+window.display = display;
+
 function nextQuestion() {
-    // starts timer
-    // countdown();
-    // display question
-    console.log(trivia.answers.q2);
-    for (var key in trivia.questions) {
-        // if (!trivia.questions.hasOwnProperty(key)) continue;
-        var obj = trivia.questions[key];
-
-    }
-    // display options
-
-    // onclick of option...to invoke answerChecker
-
+  var questionHTML = "";
+  for (var i = 0; i < trivia.question.length; i++) {
+    questionHTML += display(trivia.question[i]);
+  }
+  $("#questions-container").append(questionHTML);
+  // starts timer
+  // countdown();
+  // display question
+  // display options
+  // onclick of option...to invoke answerChecker
 }
 
 function answerChecker() {
-    console.log("answerChecker invoked")
-    // countdown() to check time remaining
-    // invoke right or wrong()
-    // displays message/pop-up depending
-    // loops to next question on setInterveral() of 5 secs
+  console.log("answerChecker invoked");
+  // countdown() to check time remaining
+  // invoke right or wrong()
+  // displays message/pop-up depending
+  // loops to next question on setInterveral() of 5 secs
 }
 
 function rightAnswer() {
-    // if right(){message Yay}
+  // if right(){message Yay}
 }
 
 function wrongAnswer() {
-    // if wrong(){message Boo}
+  // if wrong(){message Boo}
 }
 
 function results() {
-    // timer stops
-    // number of correct answers
-    // number of wrong answers
-    // start over to reset game, not page
-
+  // timer stops
+  // number of correct answers
+  // number of wrong answers
+  // start over to reset game, not page
 }
 
 // Questions...
